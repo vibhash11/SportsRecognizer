a95ab24c935c9f43dbfab10a8a91cebf21ba4fa3
Vibhash Chandra <vibhashchandra2010@gmail.com>
2020-08-09 12:50:08 +0530

    Update SportsRecognizer.py

--- SportsRecognizer.py
+++ SportsRecognizer.py
@@ -3,24 +3,29 @@ from PIL import Image
 import Testing
 import streamlit as st
 
-preTrainedExists = True
-model_path = './model/'
-st.sidebar.title("About")
 
-st.sidebar.info("Sports Recognizer.")
-st.sidebar.info("Can only recognize between Baseball, Basketball, Cricket, Football, Hockey, Tennis.")
+def main():
+    preTrainedExists = True
+    model_path = './model/'
+    st.sidebar.title("About")
 
-st.sidebar.title("Predict New Images")
-user_input_url = st.sidebar.text_input("Enter URL to Image here!","")
+    st.sidebar.info("Sports Recognizer.")
+    st.sidebar.info("Can only recognize between Baseball, Basketball, Cricket, Football, Hockey, Tennis.")
 
-st.write("Enter an image url from the left. You'll be able to view the image.")
-st.write("When you're ready, submit a prediction on the left.") 
+    st.sidebar.title("Predict New Images")
+    user_input_url = st.sidebar.text_input("Enter URL to Image here!","")
 
-if user_input_url != "":
-    path, _ = urllib.request.urlretrieve(user_input_url, "./tempFile")
-    image = Image.open(path)
-    st.image(image, caption = "Let's predict the sports!", width = 640)
+    st.write("Enter an image url from the left. You'll be able to view the image.")
+    st.write("When you're ready, submit a prediction on the left.") 
 
-if st.sidebar.button('Predict Sports'):
-    prediction, confidence = Testing.predict(model_path, path) ### EDIT
-    st.write("They are playing {}! Confidence = {:.2f}%.".format(prediction, confidence*100))
+    if user_input_url != "":
+        path, _ = urllib.request.urlretrieve(user_input_url, "./tempFile")
+        image = Image.open(path)
+        st.image(image, caption = "Let's predict the sports!", width = 640)
+
+    if st.sidebar.button('Predict Sports'):
+        prediction, confidence = Testing.predict(model_path, path) ### EDIT
+        st.write("They are playing {}! Confidence = {:.2f}%.".format(prediction, confidence*100))
+
+if __name__ == '__main__':
+    main()
