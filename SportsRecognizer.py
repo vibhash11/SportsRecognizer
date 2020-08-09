f3de9825c412b8ef00e87721a98ce2417a0fa892
Vibhash Chandra <vibhashchandra2010@gmail.com>
2020-08-09 14:42:41 +0530

    Error Handling, Concurrent Requests

--- SportsRecognizer.py
+++ SportsRecognizer.py
@@ -1,8 +1,8 @@
 import urllib.request
-from PIL import Image
+from PIL import Image as PImage
 import Testing
 import streamlit as st
-
+from torchvision import transforms
 
 def main():
     preTrainedExists = True
@@ -18,14 +18,18 @@ def main():
     st.write("Enter an image url from the left. You'll be able to view the image.")
     st.write("When you're ready, submit a prediction on the left.") 
 
+    image_tensor = ""
     if user_input_url != "":
-        path, _ = urllib.request.urlretrieve(user_input_url, "./tempFile")
-        image = Image.open(path)
-        st.image(image, caption = "Let's predict the sports!", width = 640)
-
-    if st.sidebar.button('Predict Sports'):
-        prediction, confidence = Testing.predict(model_path, path) ### EDIT
-        st.write("They are playing {}! Confidence = {:.2f}%.".format(prediction, confidence*100))
+        try:
+            image = PImage.open(urllib.request.urlopen(user_input_url))
+            image_tensor = transforms.ToTensor()(image)
+            st.image(image, caption = "Let's predict the sports!", width = 640)
+            if st.sidebar.button('Predict Sports'):
+                prediction, confidence = Testing.predict(model_path, image_tensor) ### EDIT
+                st.write("They are playing {}! Confidence = {:.2f}%.".format(prediction, confidence*100))
+        except Exception as e:
+            st.write("Invalid Input: " + str(e))
+            
 
 if __name__ == '__main__':
     main()
--- Testing.py
+++ Testing.py
@@ -1,10 +1,9 @@
 from fastai.vision import *
 
-def predict(model_path, filePath):
+def predict(model_path, image_tensor):
     defaults.device = torch.device('cpu')
-    img = open_image(Path(filePath))
     learn = load_learner(model_path)
-    pred_class, pred_idx, outputs = learn.predict(img)
+    pred_class, pred_idx, outputs = learn.predict(Image(image_tensor))
     return pred_class.obj, outputs[pred_idx]
 
 if __name__ == "__predict__":
